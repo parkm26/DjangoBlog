89c08a2d22ac5ab41f84c2d3a04ee6744ac2c8c7
Micah Park <parkm26@uw.edu>
2021-06-06 15:54:31 -0700

    Moved files to approriate locations

--- djangoenv/mysite/polling/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from polling.views import PollListView, PollDetailView
-
-urlpatterns = [
-    path("", PollListView.as_view(), name="poll_index"),
-    path("polls/<int:pk>/", PollDetailView.as_view(), name="poll_detail"),
-]
